21626af40a703fc4443ed3f256134fb54fb93317
bgr8594 <bgr8594@gmail.com>
2022-08-15 21:59:10 -0500

    pagina presupuesto
    presupuesto

---
+++ ionic-basic/src/app/service/presupuesto.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { gasto } from './gasto.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PresupuestoService {
+
+  gastos: gasto[]=[];
+  
+  constructor() { }
+
+  public agregarGasto(monto: number, gasto: string){
+    this.gastos.push({monto,gasto});
+  }
+
+  public mostrarGastos() {
+    return this.gastos;
+  }
+}

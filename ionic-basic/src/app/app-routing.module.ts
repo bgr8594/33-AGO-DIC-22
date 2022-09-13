a16a6c4f8db227e3ef284bd56ac314c578e37a8a
R2D2RDZ <R2D2RDZ.secenas@gmail.com>
2022-09-12 20:57:10 -0500

    practica 05

--- ionic-basic/src/app/app-routing.module.ts
+++ ionic-basic/src/app/app-routing.module.ts
@@ -38,6 +38,10 @@ const routes: Routes = [
     path: 'receptor',
     loadChildren: () => import('./receptor/receptor.module').then( m => m.ReceptorPageModule)
   },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
+  },
 
 
 ];

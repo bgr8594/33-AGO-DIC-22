fd6426137f397346942660d6b5bc194055cd7349
JoseLuis35 <jlpg5sep@gmail.com>
2022-09-23 03:51:26 -0500

    Práctica 06
    Navegación con Tabs, Menu flotante, Componente global para compartir componentes con toda la aplicación

--- ionic-basic/src/app/app.module.ts
+++ ionic-basic/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { ComponentesModule } from './componentes/componentes.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+  imports: [ComponentesModule,BrowserModule, IonicModule.forRoot(), AppRoutingModule,
     HttpClientModule],
   providers: [HttpClientModule, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],

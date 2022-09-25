877e44661be30c8b7e56cce52fddf4617a993f3d
jesusvrdz <jesus.vallejoro@uanl.edu.mx>
2022-09-25 16:26:46 -0500

    Practica_05_LAM

--- ionic-basic/src/app/detalle-receta/detalle-receta.page.ts
+++ ionic-basic/src/app/detalle-receta/detalle-receta.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { Receta } from '../models/receta.model';
+import { RecetaService } from '../services/receta.service';
 @Component({
   selector: 'app-detalle-receta',
   templateUrl: './detalle-receta.page.html',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalleRecetaPage implements OnInit {
 
-  constructor() { }
+  idReceta: number;
+  receta: Receta;
+
+  constructor(private recetaService: RecetaService,
+    private router: Router, private activateRouter: ActivatedRoute) { }
 
   ngOnInit() {
+
+    this.activateRouter.paramMap.subscribe(paramMap=>{
+      this.idReceta = Number.parseInt(paramMap.get('idReceta'));
+      this.receta = this.recetaService.getReceta(this.idReceta);
+    });
+
   }
 
 }

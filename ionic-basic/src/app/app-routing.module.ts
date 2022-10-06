a5e8de7098f6d146935d92fc44fde79419dc42c8
orlandova939 <orlandova939@gmail.com>
2022-10-06 06:29:38 -0500

    Practica 3

--- ionic-basic/src/app/app-routing.module.ts
+++ ionic-basic/src/app/app-routing.module.ts
@@ -14,9 +14,12 @@ const routes: Routes = [
   {
     path: 'presupuesto',
     loadChildren: () => import('./presupuesto/presupuesto.module').then( m => m.PresupuestoPageModule)
+  },  {
+    path: 'alumnos',
+    loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule)
   },
-];   
 
+];
 
 @NgModule({
   imports: [

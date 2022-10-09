676442b4ce4f7769293567c3dc1829ab3c9c8028
JoseLuis35 <jlpg5sep@gmail.com>
2022-10-09 13:22:55 -0500

    Práctica 08
    Validación de formularios, estilos globales scss

--- ionic-basic/src/app/service/aut-guard.guard.ts
+++ ionic-basic/src/app/service/aut-guard.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { AuthService } from './autservice.service';
 import { Observable } from 'rxjs';
 

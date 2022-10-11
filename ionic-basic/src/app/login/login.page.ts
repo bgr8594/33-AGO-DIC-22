f048deb9437d7b613500ae32c6f5f18ada691048
Aguilerade <adela.aguileranrg@uanl.edu.mx>
2022-10-10 21:34:06 -0500

    Practica 8
    Validacion de formularios, estilos globales scss

--- ionic-basic/src/app/login/login.page.ts
+++ ionic-basic/src/app/login/login.page.ts
@@ -4,6 +4,7 @@ import { ModalErrorComponent } from '../componentes/modal-error/modal-error.comp
 import { ModalController } from '@ionic/angular';
 import { AuthService } from '../service/autservice.service';
 import { Router } from '@angular/router';
+import {FormGroup, FormBuilder, Validators, FormControl, AbstractControl} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -13,41 +14,80 @@ import { Router } from '@angular/router';
 export class LoginPage implements OnInit {
 
   user: User = new User();
+  ionicForm: FormGroup;
 
-  constructor(private router: Router,
+  constructor(
+    private router: Router,
     private modalCtrl: ModalController,
-    private autSvc: AuthService) { }
 
-  ngOnInit() {
+private autSvc: AuthService,
+private formBuilder: FormBuilder
+) { }
+
+ngOnInit() {
+this.buildForm();
+}
+
+hasError: any = (controlName: string, errorName: string) => {
+return !this.ionicForm.controls[controlName].valid &&
+  this.ionicForm.controls[controlName].hasError(errorName) &&
+  this.ionicForm.controls[controlName].touched;
+}
+
+
+notZero(control: AbstractControl) {
+if (control.value && control.value.monto <= 0) {
+  return { 'notZero': true };
+}
+return null;
+} 
+
+async onLogin(){
+this.autSvc.onLogin(this.user).then((user:any)=>{
+  if(user!=null && user.code ==undefined){
+    console.log('Successfully logged in!');
+    this.router.navigate(['/presupuesto']);
   }
-
-  async onLogin(){
-    this.autSvc.onLogin(this.user).then((user:any)=>{
-      if(user!=null && user.code ==undefined){
-        console.log('Successfully logged in!');
-        this.router.navigate(['/presupuesto']);
+  else{
+    if(user.code){
+      if(user.code=='auth/wrong-password' || user.code =='auth/invalid-email' || user.code=='auth/argument-error'){
+        this.openModal(user);
       }
-      else{
-        if(user.code){
-          if(user.code=='auth/wrong-password' || user.code =='auth/invalid-email' || user.code=='auth/argument-error'){
-            this.openModal(user);
-          }
-        }
-      }
-    }).catch((error: any)=>{
-      this.openModal(error);
-    })
-
+    }
   }
+}).catch((error: any)=>{
+  this.openModal(error);
+})
 
-  async openModal(user: any){
-    const modal = await this.modalCtrl.create({
-      component: ModalErrorComponent,
-      componentProps:{
-        error: 'Ingres password y/o contraseña'
-      }
-    });
-    return await modal.present();
+}
+
+submitForm(){
+if(this.ionicForm.valid){
+  this.user.email = this.ionicForm.get('email').value;
+  this.user.password = this.ionicForm.get('password').value;
+  this.onLogin();
+}
+}
+
+ionViewWillEnter(){
+this.ionicForm.reset();
+}
+
+async openModal(user: any){
+const modal = await this.modalCtrl.create({
+  component: ModalErrorComponent,
+  componentProps:{
+    error: 'Ingres password y/o contraseña'
   }
+});
+return await modal.present();
+}
+
+buildForm(){
+this.ionicForm = this.formBuilder.group({
+  email: new FormControl('',{validators: [Validators.email,Validators.required]}),
+  password: new FormControl('', {validators: [Validators.required, Validators.minLength(6), Validators.maxLength(6)]})
+});
+}
 
 }

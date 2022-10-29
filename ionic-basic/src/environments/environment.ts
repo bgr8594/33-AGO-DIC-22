e8c05aa68dfb324dd385b256f83e47221250f198
JoseLuis35 <jlpg5sep@gmail.com>
2022-10-29 01:55:09 -0500

    Práctica 12

--- ionic-basic/src/environments/environment.ts
+++ ionic-basic/src/environments/environment.ts
@@ -12,7 +12,8 @@ export const environment = {
     messagingSenderId: "836492098737",
     appId: "1:836492098737:web:950d3fcf09989e2413f690",
     measurementId: "G-5PHZW4L388"
-  }
+  },
+  apiKeyMaps: "AIzaSyCeZzZJzp3zyMWFGLhqYrZuX1127hqH4H8"
 };
 
 /*

a5c11b86f007752887e256fc520f9b0459415da6
Vart00325 <york.lalito24@outlook.com>
2022-11-02 23:54:07 -0600

    practica 6

--- ionic-basic/src/app/app-routing.module.ts
+++ ionic-basic/src/app/app-routing.module.ts
@@ -31,6 +31,10 @@ const routes: Routes = [
     path: 'receta',
     loadChildren: () => import('./receta/receta.module').then( m => m.RecetaPageModule)
   },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
+  },
 ];
 
 @NgModule({

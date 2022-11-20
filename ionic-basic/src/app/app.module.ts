13d4a673b31a62f75540387f2dbdaf42bab4cc89
JoseLuis35 <jlpg5sep@gmail.com>
2022-11-20 15:59:28 -0600

    Práctica 15
    Uso de LocalStorage para guardar datos en navegador

--- ionic-basic/src/app/app.module.ts
+++ ionic-basic/src/app/app.module.ts
@@ -6,11 +6,18 @@ import { IonicModule, IonicRouteStrategy, NavParams } from '@ionic/angular';
 import { ComponentesModule } from './componentes/componentes.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { Drivers, Storage } from '@ionic/storage';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [ComponentesModule,BrowserModule, IonicModule.forRoot(), AppRoutingModule,
-    HttpClientModule],
+    HttpClientModule,
+    IonicStorageModule.forRoot({
+      name: '__mydb',
+      driverOrder: [ Drivers.LocalStorage]
+    })
+  ],
   providers: [NavParams, HttpClientModule, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })

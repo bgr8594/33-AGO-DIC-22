92d5024198a2cbbca5a6f804568daf4fa443570b
Aguilerade <adela.aguileranrg@uanl.edu.mx>
2022-11-24 16:50:08 -0600

    Practica 9
    Alta lugar en firestore, consulta de lugar una sola ves, evento de volver a pagina destinos

--- ionic-basic/src/app/service/autservice.service.ts
+++ ionic-basic/src/app/service/autservice.service.ts
@@ -4,8 +4,12 @@ import { initializeApp } from "firebase/app"
 import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { User } from '../models/user.model';
+import { getFirestore, collection, addDoc, getDocs } from 'firebase/firestore';
+import { Lugar } from '../models/lugar.model';
+import { getDatabase } from "firebase/database";
 
 const firebaseApp = initializeApp(environment.firebaseConfig);
+const dbCloudFirestore = getFirestore(firebaseApp);
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +19,7 @@ export class AuthService {
   public isLoged : any = false;
   auth: Auth;
 
+  db = dbCloudFirestore; 
   constructor() {
     this.auth = getAuth(firebaseApp);
     onAuthStateChanged(this.auth, user => {
@@ -39,4 +44,30 @@ export class AuthService {
    onRegister(user: User): Promise<any>{
       return  createUserWithEmailAndPassword(this.auth, user.email, user.password);
   }     
+
+async altaLugar(lugar: Lugar){
+  const lugarTemp: any ={
+    nombre:lugar.nombre,
+    ubicacion: {longitud:'', latitud:''}
+  };
+  const docRef = await addDoc(collection(this.db,'lugar'), lugarTemp);
+  console.log("Documento escrito con id: "+docRef.id);
+}
+
+async getLugares(destinos: Lugar[]) {
+  await getDocs(collection(this.db, 'lugar'))
+  .then((querySnapshot: any)=>{
+    destinos.splice(0, destinos.length);
+    querySnapshot.forEach((doc)=>{
+      let data: any = doc.data();
+        let lugar: Lugar = new Lugar();
+        lugar.nombre = data.nombre;
+        console.log(doc.id);
+        destinos.push(lugar);
+    });
+  })
+  .catch(error=>{
+    console.log('Ocurrio un error en el guardardo:'+error);
+  });
+}
 }

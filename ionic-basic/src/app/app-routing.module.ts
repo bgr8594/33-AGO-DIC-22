b3b751f76b8c2f6190de80954e113435ddb860f4
gameOz1 <ozmarf00@gmail.com>
2022-11-25 20:03:56 -0600

    Practica 10/9

--- ionic-basic/src/app/app-routing.module.ts
+++ ionic-basic/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { AutGuardGuard } from './service/aut-guard.guard';
 
 const routes: Routes = [
   {
@@ -14,59 +13,53 @@ const routes: Routes = [
   },
   {
     path: 'presupuesto',
-    loadChildren: () => import('./presupuesto/presupuesto.module').then( m => m.PresupuestoPageModule),
-    canActivate: [AutGuardGuard]
+    loadChildren: () => import('./presupuesto/presupuesto.module').then( m => m.PresupuestoPageModule)
   },
   {
     path: 'alumnos',
-    loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule),
-    canActivate: [AutGuardGuard]
+    loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule)
   },
   {
     path: 'inicio',
-    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule),
-    canActivate: [AutGuardGuard]
-
+    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
   },
   {
     path: 'receptor',
-    loadChildren: () => import('./receptor/receptor.module').then( m => m.ReceptorPageModule),
-    canActivate: [AutGuardGuard]
+    loadChildren: () => import('./receptor/receptor.module').then( m => m.ReceptorPageModule)
   },
   {
     path: 'detalle-receta',
-    loadChildren: () => import('./detalle-receta/detalle-receta.module').then( m => m.DetalleRecetaPageModule),
-    canActivate: [AutGuardGuard]
+    loadChildren: () => import('./detalle-receta/detalle-receta.module').then( m => m.DetalleRecetaPageModule)
   },
   {
     path: 'receta',
-    loadChildren: () => import('./receta/receta.module').then( m => m.RecetaPageModule),
-    canActivate: [AutGuardGuard]
+    loadChildren: () => import('./receta/receta.module').then( m => m.RecetaPageModule)
   },
   {
     path: 'tabs',
-    loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule),
-    canActivate: [AutGuardGuard]
+    loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then( m => m.AdminPageModule)
   },
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
   {
-    path: 'admin',
-    loadChildren: () => import('./admin/admin.module').then( m => m.AdminPageModule),
-    canActivate: [AutGuardGuard]
-  },
-  {
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   },
   {
+    path: 'borrar',
+    loadChildren: () => import('./borrar/borrar.module').then( m => m.BORRARPageModule)
+  },
+  {
     path: 'destinos',
-    loadChildren: () => import('./destinos/destinos.module').then( m => m.DestinosPageModule),
-    canActivate: [AutGuardGuard]
-  }
- 
+    loadChildren: () => import('./destinos/destinos.module').then( m => m.DestinosPageModule)
+  },
+
 ];
 
 @NgModule({
--- ionic-basic/src/app/inicio/inicio.page.ts
+++ ionic-basic/src/app/inicio/inicio.page.ts
@@ -1,33 +1,33 @@
 import { Component, OnInit } from '@angular/core';
- import { EnvioReceptorService } from '../service/envio-receptor.service';
- import { Router } from '@angular/router';
+import { EnvioReceptorService } from '../service/envio-receptor.service';
+import { Router } from '@angular/router';
 
- @Component({
-   selector: 'app-inicio',
-   templateUrl: './inicio.page.html',
-   styleUrls: ['./inicio.page.scss'],
- })
- export class InicioPage implements OnInit {
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
 
-   user = {nombre:'Pedro Perez', uuid:"34523452345234523452345", email:"correo@gmail.com"};
+  user = {nombre:'Pedro Perez', uuid:"34523452345234523452345", email:"correo@gmail.com"};
 
-   list: any[]=
-   [
-     {nombre:'Pedro Perez', uuid:"34523452345234523452345", email:"correo@gmail.com"},
-     {nombre:'Pedro Perez', uuid:"34523452345234523452345", email:"correo@gmail.com"},
-     {nombre:'Pedro Perez', uuid:"34523452345234523452345", email:"correo@gmail.com"}
-   ];
+  list: any[]=
+  [
+    {nombre:'Pedro Perez', uuid:"34523452345234523452345", email:"correo@gmail.com"},
+    {nombre:'Pedro Perez', uuid:"34523452345234523452345", email:"correo@gmail.com"},
+    {nombre:'Pedro Perez', uuid:"34523452345234523452345", email:"correo@gmail.com"}
+  ];
 
-   constructor(private router: Router, private envioReceptor: EnvioReceptorService) { }
+  constructor(private router: Router, private envioReceptor: EnvioReceptorService) { }
 
-   ngOnInit() {
-   }
+  ngOnInit() {
+  }
 
-   gotReceiver(){
-     this.envioReceptor.sendObjectSource(this.user);
-     this.envioReceptor.sendListSource(this.list);
+  gotReceiver(){
+    this.envioReceptor.sendObjectSource(this.user);
+    this.envioReceptor.sendListSource(this.list);
 
-     this.router.navigate(['/receptor']);
-   }
+    this.router.navigate(['main/receptor']);
+  }
 
- }
+}

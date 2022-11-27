fe7211378f885f81dd2aac3a083a4e75b7f4aaff
MasterLuigiRdz <luiz01rdz@hotmail.com>
2022-11-26 20:28:10 -0600

    Práctica 8

--- ionic-basic/src/app/register/register.page.ts
+++ ionic-basic/src/app/register/register.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../service/autservice.service';
 import { Router } from '@angular/router';
 import { User } from '../models/user.model';
+import { FormBuilder, FormGroup, Validators, FormControl, AbstractControl } from '@angular/forms';
+import { ModalController } from '@ionic/angular';
+import { ModalErrorComponent } from '../componentes/modal-error/modal-error.component';
 
 @Component({
   selector: 'app-register',
@@ -11,13 +14,33 @@ import { User } from '../models/user.model';
 export class RegisterPage implements OnInit {
 
   user: User = new User();
+  formRegister!: FormGroup;
 
   constructor(
     private autSvc: AuthService,
-    private router: Router
-    ) { }
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private modalCtrl: ModalController
+    ) {
+      this.buildForm();
+      this.formRegister.reset();
+     }
 
   ngOnInit() {
+    this.formRegister.reset();
+  }
+
+
+  submitForm(){
+    if(this.formRegister.valid){
+      this.user.email = this.formRegister.get('email').value;
+      this.user.password = this.formRegister.get('password').value;
+      this.onRegister();
+    }
+  }
+
+  ionViewWillEnter(){
+    this.formRegister.reset();
   }
 
   async onRegister(){
@@ -27,9 +50,35 @@ export class RegisterPage implements OnInit {
         this.router.navigate(['/login']);
       }
     }).catch(error=>{
-      console.log('Error al crear usuario!');
+      if(error.code =='auth/email-already-in-use'){
+        this.openModal(error);
+      }
+      console.log(error.code);
     })
 
   } 
 
+  hasError: any = (controlName: string, errorName: string) => {
+		return !this.formRegister.controls[controlName].valid &&
+			this.formRegister.controls[controlName].hasError(errorName) &&
+			this.formRegister.controls[controlName].touched;
+	}
+
+  buildForm(){
+    this.formRegister = this.formBuilder.group({
+      email: new FormControl('',{validators: [Validators.email,Validators.required]}),
+      password: new FormControl('', {validators: [Validators.required, Validators.minLength(6), Validators.maxLength(6)]})
+    });
+  }
+
+
+  async openModal(user: any){
+    const modal = await this.modalCtrl.create({
+      component: ModalErrorComponent,
+      componentProps:{
+        error: 'Error al crear el usuario'
+      }
+    });
+    return await modal.present();
+  }
 }

1b4267cac34f2581625b4c2b5eaa62565245ce05
MasterLuigiRdz <luiz01rdz@hotmail.com>
2022-11-26 20:02:18 -0600

    Práctica 7

--- ionic-basic/src/environments/environment.ts
+++ ionic-basic/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCeXXaPLy-4EGz1u1NEKiw8sH9PrVarTe4",
+    authDomain: "lab-apps-da679.firebaseapp.com",
+    databaseURL: "https://lab-apps-da679-default-rtdb.firebaseio.com",
+    projectId: "lab-apps-da679",
+    storageBucket: "lab-apps-da679.appspot.com",
+    messagingSenderId: "104636534220",
+    appId: "1:104636534220:web:d8c139aad237a279ef2169"
+  }
 };
 
 /*

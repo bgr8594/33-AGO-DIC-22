dc57c96ed53b895cdfc000350356dcdb53ccc433
orlandova939 <orlandova939@gmail.com>
2022-11-26 22:53:41 -0600

    Practica 11

--- ionic-basic/src/app/service/autservice.service.ts
+++ ionic-basic/src/app/service/autservice.service.ts
@@ -31,7 +31,7 @@ export class AuthService {
    tieneSesion(){
     return this.isLoged;
    }
-   
+
    getStateAuth(){
     return this.auth;
    }
@@ -47,7 +47,8 @@ export class AuthService {
   async altaLugar(lugar: Lugar){
     const lugarTemp: any ={
       nombre:lugar.nombre,
-      ubicacion: {longitud:'', latitud:''}
+      latitud: lugar.latitud,
+      longitud: lugar.longitud
     };
     const docRef = await addDoc(collection(this.db,'lugar'), lugarTemp);
     console.log("Documento escrito con id: "+docRef.id);
@@ -61,6 +62,8 @@ export class AuthService {
         let data: any = doc.data();
           let lugar: Lugar = new Lugar();
           lugar.nombre = data.nombre;
+          lugar.latitud = data.latitud;
+          lugar.longitud = data.longitud;
           lugar.id = doc.id;
           console.log(doc.id);
           destinos.push(lugar);
@@ -74,7 +77,8 @@ export class AuthService {
   updateLugares(id: any, lugar: any): Promise<any>{
     const docRef = doc(this.db, 'lugar', id);
     const lugarAux = {nombre: lugar.nombre,
-      ubicacion:{latitud:'', longitud:''}
+      latitud: lugar.latitud,
+      longitud: lugar.longitud
     };
 
     return setDoc(docRef, lugarAux);
